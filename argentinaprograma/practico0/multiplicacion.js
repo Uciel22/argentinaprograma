3a6cfd16be419335efb7aa4b6da11532097e22ed
Uciel22 <ucielibarra@gmail.com>
2023-05-26 19:43:45 -0300

    agrego resta

---
+++ argentinaprograma/practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+let num1 = num1;
+let num2 = num2;
+let num3 = num3;
+let multiplicacion = num1 * num2 * num3;
+console.log(multiplicacion);
---
+++ argentinaprograma/practico0/resta.js
@@ -0,0 +1,4 @@
+let minuendo = num1;
+let sustraendo = num2;
+let resultado = num1 - num2;
+console.log(resultado);
