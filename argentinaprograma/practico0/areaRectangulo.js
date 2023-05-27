e451f147dec03964b10a9b40965c6a8ccbd6dd2e
Uciel22 <ucielibarra@gmail.com>
2023-05-26 22:55:34 -0300

    Practico N0 finalizado

--- argentinaprograma/practico0/areaRectangulo.js
+++ argentinaprograma/practico0/areaRectangulo.js
@@ -2,3 +2,4 @@ function calcularArea(base = 0, altura = 0){
     return base * altura;
 }
 console.log(calcularArea(5, 3))
+console.log(calcularArea(12.5, 5.3));
---
+++ argentinaprograma/practico0/promedio3.js
@@ -0,0 +1,6 @@
+function calcularPromedio(num1, num2, num3){
+ let promedio = (num1 + num2 + num3) /3;
+return promedio}
+ console.log(calcularPromedio(5, 7, 9));
+ console.log(calcularPromedio(-2, -3, -8));
+ console.log(calcularPromedio(-1.2, 5.3, -3.3));
