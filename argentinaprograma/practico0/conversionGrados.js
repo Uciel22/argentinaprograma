4ca331a8c4da7dbe262a31b36628fc2504c8757e
Uciel22 <ucielibarra@gmail.com>
2023-05-26 22:26:21 -0300

    Agrego el archivo conversionGrados.js

---
+++ argentinaprograma/practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+function conversionGradosF(fahrenheit){
+    let celsius = (fahrenheit - 32) * 5 / 9.0
+    return celsius;
+}
+console.log(conversionGradosF(50));
+console.log(conversionGradosF(75));
