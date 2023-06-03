a82fef5c44ad489ad0f9f9d004ace55695429408
Uciel22 <ucielibarra@gmail.com>
2023-06-03 20:51:31 -0300

    agrego archivo resta.js modificado

--- argentinaprograma/practico0/resta.js
+++ argentinaprograma/practico0/resta.js
@@ -2,3 +2,7 @@ let minuendo = num1;
 let sustraendo = num2;
 let resultado = num1 - num2;
 console.log(resultado);
+// Respuesta practico0 punto 1.a
+let num1 = 10;
+let num2 = 2;
+console.log(resultado)
