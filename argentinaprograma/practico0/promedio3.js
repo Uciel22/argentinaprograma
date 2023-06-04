e2244102412cda99966f7c8009efc0fd91e597d1
Uciel22 <ucielibarra@gmail.com>
2023-06-03 21:48:57 -0300

    archivo promedio3.js modificado

--- argentinaprograma/practico0/promedio3.js
+++ argentinaprograma/practico0/promedio3.js
@@ -1,6 +1,12 @@
-function calcularPromedio(num1, num2, num3){
- let promedio = (num1 + num2 + num3) /3;
+function calcularPromedio(x, y, z){
+ let promedio = (x + y + z) /3;
 return promedio}
+
+//Respuesta practico0 punto 4.a
  console.log(calcularPromedio(5, 7, 9));
+
+ //Respuesta practico0 punto 4.b
  console.log(calcularPromedio(-2, -3, -8));
+
+ //Respuesta practico0 punto 4.c
  console.log(calcularPromedio(-1.2, 5.3, -3.3));
