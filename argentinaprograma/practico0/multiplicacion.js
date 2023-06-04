4291a04e755c8f42cf8b97c2dedacca6fbc6ab17
Uciel22 <ucielibarra@gmail.com>
2023-06-03 21:34:18 -0300

    archivo multiplicacion.js modificado

--- argentinaprograma/practico0/multiplicacion.js
+++ argentinaprograma/practico0/multiplicacion.js
@@ -3,3 +3,9 @@ let num2 = num2;
 let num3 = num3;
 let multiplicacion = num1 * num2 * num3;
 console.log(multiplicacion);
+
+// Respuesta al practico0 punto 1.b
+let num1 = 2;
+let num2 = 17;
+let num3 = 6;
+console.log(multiplicacion);
