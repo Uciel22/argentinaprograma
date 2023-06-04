c7b0d7d8f6f38ed28092b1e8f4caefae8d13a485
Uciel22 <ucielibarra@gmail.com>
2023-06-03 21:39:07 -0300

    archivo areaRectangulo.js modificado

--- argentinaprograma/practico0/areaRectangulo.js
+++ argentinaprograma/practico0/areaRectangulo.js
@@ -1,5 +1,9 @@
 function calcularArea(base = 0, altura = 0){
     return base * altura;
 }
+
+//Respuesta practico0 punto 2.a
 console.log(calcularArea(5, 3))
+
+//Respuesta practica0 punto 2.b
 console.log(calcularArea(12.5, 5.3));
