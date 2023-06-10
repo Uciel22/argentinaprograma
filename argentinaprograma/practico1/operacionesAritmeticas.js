f38ccab373497b0eb438aa44b175262b52708765
Uciel22 <ucielibarra@gmail.com>
2023-06-10 20:18:35 -0300

    agrego el archivo operacionesAritmeticas.js

---
+++ argentinaprograma/practico1/operacionesAritmeticas.js
@@ -0,0 +1,21 @@
+// Practico1 Parte 2 punto 3.a
+let num1 = 2;
+let num2 = 2;
+let num3 = 4;
+let num4 = 15;
+
+// Practico1 Parte 2 punto 3.b
+let suma = num1 + num2;
+
+// Practico1 Parte 2 punto 3.c
+let resta = num4 - num3;
+
+// Practico1 Parte 2 punto 3.d
+let resultadoFinal = suma * resta;
+
+// Practico1 Parte 2 punto 3.e
+if ((resultadoFinal % 2)=== 0){ esPar = true};
+let esPar;
+
+// Practico 1 Parte 2 punto 3.f
+console.log("Mis variables iniciales fueron: num1, num2, num3 y num4. La respuesta a verificar si el resultado final es par es: esPar");
