8e223af4340b73d935c000725a4ea6c02cfa7625
Uciel22 <ucielibarra@gmail.com>
2023-06-10 20:15:05 -0300

    agrego el archivo frutas.js

---
+++ argentinaprograma/practico1/frutas.js
@@ -0,0 +1,11 @@
+// Practico1 Parte 2 punto 5.a y b
+let frutas = [];
+frutas = ["Manzana", "Banana", "Pera", "Limon", "Naranja"];
+
+//Punto 5.c
+let ultima = frutas[4];
+console.log("La ultima fruta agregada fue: " + ultima);
+
+//Punto 5.d
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: "+ primera);
