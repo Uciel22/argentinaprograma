94ed8c6ed0692ee355688eb85c02da39c4489f55
Uciel22 <ucielibarra@gmail.com>
2023-06-18 18:53:37 -0300

    agrego archivo mayorDeEdad.js

--- argentinaprograma/practico2/mayorDeEdad.js
+++ argentinaprograma/practico2/mayorDeEdad.js
@@ -1,2 +1,9 @@
 //Practico 2 punto 1.a
 const readlineSync = require("readline-sync");
+
+const edad = readlineSync.question("¿Cuál es tu edad?");
+if (edad >= 18){
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
