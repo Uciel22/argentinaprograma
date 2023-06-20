75faeb3867992d1c6c6bf743c94e195fad6f9aec
Uciel22 <ucielibarra@gmail.com>
2023-06-19 22:18:43 -0300

    archivo meses.js

--- argentinaprograma/practico2/meses.js
+++ argentinaprograma/practico2/meses.js
@@ -1,1 +1,11 @@
 // Practico N2 punto 2
+
+const readlineSync = require("readline-sync");
+
+let mes = readlineSync.question("Ingrese un numero del 1 al 12");
+
+mes = [0, "enero","febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+let cantidadDeDias = [0, "31", "28","31","30","31","30","31","31","30","31","30","31"];
+
+console.log("La cantidad de dias del mes de" + mes[1] + "es " + cantidadDeDias[1]);
