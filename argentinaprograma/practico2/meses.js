2ba0186c2aac9c75f242c11de4ebb047230db6ce
Uciel22 <ucielibarra@gmail.com>
2023-06-21 22:25:19 -0300

    modifico archivo meses.js

--- argentinaprograma/practico2/meses.js
+++ argentinaprograma/practico2/meses.js
@@ -2,10 +2,75 @@
 
 const readlineSync = require("readline-sync");
 
-let mes = readlineSync.question("Ingrese un numero del 1 al 12");
+let numero = readlineSync.question("Ingrese un numero del 1 al 12");
 
-mes = [0, "enero","febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+ let mes;
 
-let cantidadDeDias = [0, "31", "28","31","30","31","30","31","31","30","31","30","31"];
+ let cantidadDeDias;
 
-console.log("La cantidad de dias del mes de" + mes[1] + "es " + cantidadDeDias[1]);
+ switch (numero){
+    case "1": 
+    mes = "Enero";
+    cantidaDeDias = 31;
+    break;
+
+    case "2":
+        mes = "Febrero";
+        cantidadDeDias = 28;
+        break;
+
+        case "3":
+            mes = "Marzo";
+            cantidadDeDias = 31;
+            break;
+
+            case "4":
+                mes = "Abril";
+                cantidadDeDias = 30;
+                break;
+
+                case "5":
+                    mes = "Mayo";
+                    cantidadDeDias = 31;
+                    break;
+
+                    case "6": 
+                    mes = "Junio";
+                    cantidadDeDias = 30;
+                    break;
+
+                    case "7":
+                        mes = "Julio";
+                        cantidadDeDias = 31;
+                        break;
+
+                        case "8":
+                            mes = "Agosto";
+                            cantidadDeDias = 31;
+                            break;
+
+                            case "9":
+                                mes = "Septiembre";
+                                cantidadDeDias = 30;
+                                break;
+                                
+                                case "10":
+                                    mes ="Octubre";
+                                    cantidadDeDias = 31;
+                                    break;
+
+                                    case "11":
+                                        mes = "Noviembre";
+                                        cantidadDeDias = 30;
+                                        break;
+
+                                        case "12": 
+                                        mes = "Diciembre";
+                                        cantidadDeDias = 31;
+                                        break;
+
+                                        default:
+                                            console.log("Por favor ingrese un numero entre 1 y 12");
+}
+
+console.log("La cantidad de dias del mes de " + mes + " es " + cantidadDeDias);
