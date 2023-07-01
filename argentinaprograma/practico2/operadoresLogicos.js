4c56d03e64449600181195d66385b709fa95145f
Uciel22 <ucielibarra@gmail.com>
2023-07-01 18:40:02 -0300

    modifico archivo operadoresLogicos.js

--- argentinaprograma/practico2/operadoresLogicos.js
+++ argentinaprograma/practico2/operadoresLogicos.js
@@ -2,12 +2,14 @@
 const readlineSync = require("readline-sync");
 
 const numeroEntero = readlineSync.question("Por favor, ingrese un numero");
-if ((numeroEntero %2) === 0 && numeroEntero >= 0) {
+if ((numeroEntero %2) === 0 && numeroEntero > 0) {
     console.log("El numero es positivo y par");
-} else if ((numeroEntero %2) !== 0 && numeroEntero >= 0) {
+} else if ((numeroEntero %2) !== 0 && numeroEntero > 0) {
     console.log("El numero es positivo e impar");
 } else if (numeroEntero < 0) {
     console.log("El numero es negativo");
-} else if (numeroEntero = 0) {
+} else {
     console.log("El numero es cero");
 }
+
+// Lo modifico según observación en corrección
