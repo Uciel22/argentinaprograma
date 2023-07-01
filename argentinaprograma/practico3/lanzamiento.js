37a39cfde8a093070d8876c9451206023b98a9d1
Uciel22 <ucielibarra@gmail.com>
2023-06-30 21:05:40 -0300

    modifico archivo lanzamiento.js

--- argentinaprograma/practico3/lanzamiento.js
+++ argentinaprograma/practico3/lanzamiento.js
@@ -1,13 +1,11 @@
 //Practico 3
 
-let i = 10;
-
-for (i = 10; i = 0; i--){
-    if (i = 10){
+for (let i = 10; i >= 0; i--){
+    if (i == 10){
         console.log("Cuenta regresiva 10");
-    } else if (i < 10 && i > 0){
-        console.log(i);
-    } else if (i = 0){
+    } else if (i == 0){
         console.log("lanzamiento!");
+    } else {
+        console.log(i);
     }
 }
